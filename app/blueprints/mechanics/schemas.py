bd6803f53089877bde7dbc8261b5744b0a68f3bd
William Schock <wschock96@gmail.com>
2025-07-22 12:14:03 -0700

    Updated Commit

--- app/blueprints/mechanics/schemas.py
+++ app/blueprints/mechanics/schemas.py
@@ -1,9 +1,14 @@
 from app.extensions import ma
 from app.models import Mechanic
+from marshmallow import fields
 
 class MechanicSchema(ma.SQLAlchemyAutoSchema):
+    tickets = fields.Method("get_ticket_ids")
     class Meta:
         model = Mechanic
+
+    def get_ticket_ids(self, obj):
+        return [ticket.id for ticket in obj.tickets]
 
 mechanic_schema = MechanicSchema()
 mechanics_schema = MechanicSchema(many=True)
