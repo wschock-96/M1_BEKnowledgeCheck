82fbb8b5ded66c6169b68379a022b2e415f10257
William Schock <wschock96@gmail.com>
2025-08-25 16:26:44 -0700

    adjusted swagger

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from app import create_app
 from app.extensions import db
+from flask import redirect
 
 app = create_app('ProductionConfig')
+
+@app.route('/', methods=['GET'])
+def index():
+    return redirect('/api/docs')
 
 with app.app_context():
     # db.drop_all()
