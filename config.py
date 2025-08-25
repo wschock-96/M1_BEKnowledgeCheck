2ae7a9a91f7419c86ee46c3d2a9ecd5e57f84f12
William Schock <wschock96@gmail.com>
2025-08-25 16:17:50 -0700

    added psycogp2

--- config.py
+++ config.py
@@ -19,5 +19,4 @@ class TestingConfig:
 class ProductionConfig:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or 'sqlite:///app.db'
     CACHE_TYPE = 'SimpleCache'
-    DEBUG = False
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
